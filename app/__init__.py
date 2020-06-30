c577b2a040276121a6c5a8d9a617101b60cfbd44
connorreitz <cjreitz18@gmail.com>
2020-06-30 19:58:35 -0400

    added email support for changing passwords, rendering complete page is still not working correctly yet

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 import logging
 import os
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
@@ -21,7 +23,7 @@ if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSOWRD'])
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
 
         secure = None
         if app.config['MAIL_USE_TLS']:
--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = 25
+    MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['cjreitz18@gmail.com']
+    ADMINS = ['reitzrecipes@gmail.com']
     POSTS_PER_PAGE = 3
